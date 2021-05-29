becd7771e6c63ab960916195f08229a9d8a666c7
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2021-05-29 11:19:41 +0430

    Fourth Challenge Solved

---
+++ 04_Day 3(Intro to Conditional Statements)/solution.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        if(N%2==0)
+        {
+        if(N>=2 && N<=5)
+            System.out.print("Not Weird");
+        else if(N>=6 && N<=20)
+            System.out.print("Weird");
+            else
+            System.out.print("Not Weird");
+    }
+    else{
+        System.out.println("Weird");
+    }
+        bufferedReader.close();
+    }
+}
+
