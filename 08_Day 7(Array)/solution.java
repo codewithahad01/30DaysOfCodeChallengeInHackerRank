a7943d4790193d4ff161d92fa1ba150925cad506
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2021-05-30 21:10:31 +0430

    Eith Challenge Solved

---
+++ 08_Day 7(Array)/solution.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+        
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arrTemp[(n - 1) - i] + " ");
+        }
+
+        bufferedReader.close();
+    }
+}
+
